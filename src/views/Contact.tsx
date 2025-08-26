a829ea396583eddf8776897e033125022a47487b
Kyle Prestado <kylerossprestado@gmail.com>
2025-08-26 20:12:17 +0800

    implement inputs in contact

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -1,7 +1,21 @@
-import React from "react";
-
 const Contact = () => {
-  return <footer></footer>;
+  return (
+    <section id="contact">
+      <p>
+        <b>Hit me up!</b>
+        <br />
+        Lorem Ipsum
+      </p>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input required name="email" />
+        <label htmlFor="email">Email</label>
+        <input required name="email" />
+        <label htmlFor="message">Message</label>
+        <textarea name="message"></textarea>
+      </form>
+    </section>
+  );
 };
 
 export default Contact;
